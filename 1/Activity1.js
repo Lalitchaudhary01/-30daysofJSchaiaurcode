f500865f8cf0e6a686df0f44563ade06ba332127
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-07-17 12:16:34 +0530

    day-1

---
+++ 1/Activity1.js
@@ -0,0 +1,6 @@
+//Acitivity 1
+
+// var int = 3;
+// console.log(int);
+let str = 5;
+console.log(str);
---
+++ 1/Activity2.js
@@ -0,0 +1,29 @@
+//task3
+// const num = 0;
+// console.log(num);
+
+//task 4
+// var int = 4;
+// let str = "Lalit";
+// const isAdult = true;
+// const array = ["Saab", "Volvo", "BMW"];
+// let myObj = {
+//   name: "LAlit",
+//   age: 18,
+// };
+// console.log(typeof int);
+// console.log(typeof str);
+// console.log(typeof isAdult);
+// console.log(typeof array);
+// console.log(typeof myObj);
+
+//task 5
+// let accountName = "Hemachoudhary"
+// console.log(accountName);
+// accountName = "HiteshChoudhary"
+// console.log(accountName);
+
+//task 6
+const lalit = 12;
+let lalit = 13;
+console.log(lalit);
